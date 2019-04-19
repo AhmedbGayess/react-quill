969c50fcc9b0937e389ebc3654939bfd48214a6d
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2019-04-19 11:16:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-class App extends Component {
+class RichEditor extends React.Component {
+  state = { text: "" };
+
+  handleChange = (value) => {
+    this.setState({ text: value });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ width: "50%", margin: "50px auto" }}>
+        <ReactQuill
+          value={this.state.text}
+          onChange={this.handleChange}
+          modules={RichEditor.modules}
+          formats={RichEditor.formats}
+        />
       </div>
     );
   }
 }
 
-export default App;
+RichEditor.modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ size: ["small", false, "large", "huge"] }],
+    [{ font: [] }],
+    [{ align: [] }],
+    [{ indent: "-1" }, { indent: "+1" }],
+    ["bold", "italic", "underline", "strike"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ color: [] }],
+    [{ direction: "rtl" }],
+    ["blockquote"],
+    ["link", "image", "video"],
+    ["clean"]
+  ],
+  clipboard: {
+    matchVisual: false
+  }
+};
+
+RichEditor.formats = [
+  "header",
+  "font",
+  "align",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+  "color"
+];
+
+export default RichEditor;
